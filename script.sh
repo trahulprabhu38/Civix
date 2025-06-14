30ef904bc170e274e3b377672f3fb22f8b2623f4
trahulprabhu38 <trahulprabhu38@gmail.com>
2025-06-15 01:02:44 +0530

    done

---
+++ script.sh
@@ -0,0 +1,9 @@
+npm install -g vercel
+
+vercel login
+
+vercel team ls
+
+
+
+# run this in local desktop to get the id , this id is to be saved in the secrets in the github. after getting the id , you can delete this file.
